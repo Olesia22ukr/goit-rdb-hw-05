15d187a2f346a2c1b07dbb773def7df12ad15108
Олеся <zykrina@ukr.net>
2025-12-07 07:20:29 +0200

    feat: add solution for task 2
    Added SQL query to select order details for a specific shipper.

--- solutions.sql
+++ solutions.sql
@@ -8,3 +8,12 @@ SELECT
     ) AS customer_id
 FROM
     order_details od;
+
+-- Task 2
+SELECT *
+FROM order_details od
+WHERE od.order_id IN (
+    SELECT o.id
+    FROM orders o
+    WHERE o.shipper_id = 3
+);
