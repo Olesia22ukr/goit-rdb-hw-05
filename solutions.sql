9eac9528192b5996289357dd4001d85e19a78dfa
Олеся <zykrina@ukr.net>
2025-12-07 07:36:29 +0200

    feat: add solution for task 4

--- solutions.sql
+++ solutions.sql
@@ -34,3 +34,21 @@ FROM
     ) AS temp
 GROUP BY
     temp.order_id;
+
+-- Task 4
+WITH temp AS (
+    SELECT
+        order_id,
+        quantity
+    FROM
+        order_details
+    WHERE
+        quantity > 10
+)
+SELECT
+    order_id,
+    AVG(quantity) AS avg_quantity
+FROM
+    temp
+GROUP BY
+    order_id;
